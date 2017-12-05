19fdb3fc152a705a51ac5af8d3c775381bc3c732
Jacob Silver <jacobsilver2@mac.com>
2017-12-05 20:05:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-
+function nestedTarget() {
+    var nested = document
+        .querySelector('div#nested div div div div.target')
+        
+    return nested;
+}
 
 function getFirstSelector(selector) {
   var element = document.querySelector(selector);

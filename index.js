51a5d0ffc13b3eb10bbc744675c9286c5ef70eea
Jacob Silver <jacobsilver2@mac.com>
2017-12-05 20:06:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ function nestedTarget() {
     return nested;
 }
 
+function increaseRankBy(n) {
+    const lis = document
+        .getElementById('app')
+        .querySelectorAll('ul.ranked-list li');
+    console.log(lis);
+    for (var i = 0; i < lis.length; i++) {
+        var num = lis[i].innerHTML
+        num = parseInt(num) + n;
+        lis[i].innerHTML = num;
+    }
+    return lis;
+}
+
 function getFirstSelector(selector) {
   var element = document.querySelector(selector);
 return element;
